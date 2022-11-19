d3510e3e517a94f55363f8dfdc44570326f85f6c
Abinash <abi.kh03@gmail.com>
2022-11-20 06:58:49 +1100

    create value on database and redirect back to show page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,5 +15,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         @article.save
+        #redirect_to article_path(@article.id) #one way
+        redirect_to @article #other way
     end
 end
