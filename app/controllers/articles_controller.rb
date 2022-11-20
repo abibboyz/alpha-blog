a0b0fb106b7ef79bd926bf5f0a563c7119ed9875
Abinash <abi.kh03@gmail.com>
2022-11-20 12:38:18 +1100

    validation  messages and success messages added and rendered to new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,18 @@ class ArticlesController < ApplicationController
     end 
 
     def new
-
+        @article = Article.new
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        #redirect_to article_path(@article.id) #one way
-        redirect_to @article #other way
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            #redirect_to article_path(@article.id) #one way
+            redirect_to @article #other way
+        else
+            render :new, status: :unprocessable_entity
+        end   
+        
     end
 end
