15875405d7a83113655d5c340b1b8f2988dc1d4a
Abinash <abi.kh03@gmail.com>
2022-11-21 08:18:56 +1100

    Crud operation Completed.

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+***************************************************************
+Restful routes for articles with resources keyboard in routes.
+***************************************************************
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "articles#index"
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
