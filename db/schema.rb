a823f51d42dffd0f214dabfe5bb36f006c65e6b5
Abinash <abi.kh03@gmail.com>
2022-11-28 22:00:36 +1100

    password field added and BCrypt introduced to secure password.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_231844) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_214028) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_231844) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
