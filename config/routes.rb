d369aa84f87db64b0c5dfcefd5762b4297975e36
Abinash <abi.kh03@gmail.com>
2022-11-29 21:51:33 +1100

    user signup form added with _form partial for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, except: [:new]
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
