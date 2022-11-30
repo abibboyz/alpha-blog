a44748e1c51b0bc8b4c44561555119254c0722db
Abinash <abi.kh03@gmail.com>
2022-11-30 22:10:12 +1100

    user profile page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,10 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @articles = @user.articles
+    end
 
+    def index
+        @users = User.all
     end
 
 
