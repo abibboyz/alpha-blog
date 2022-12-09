0483ff00b5f1932144d9b777b881949fcfc57cff
Abinash <abi.kh03@gmail.com>
2022-12-10 09:22:41 +1100

    Functional test added for create action.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
     def new
-  
+      @category = Category.new
     end
   
     def index
@@ -10,5 +10,20 @@ class CategoriesController < ApplicationController
   
     def show
   
+    end
+
+    def create 
+      @category = Category.new(category_params)
+      if @category.save
+        flash[:notice] = "Category was successfully created"
+        redirect_to @category
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+    def category_params
+      params.require(:category).permit(:name)
     end
   end
